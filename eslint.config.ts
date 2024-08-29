249763b8c0ccffcc09f15d5f01887356777d9d32
Stevie-Ray Hartog <mail@stevie-ray.nl>
2024-08-29 23:00:26 +0200

    use experimental ts eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,7 @@
-// @ts-check
-
 import eslint from "@eslint/js"
 import tseslint from "typescript-eslint"
+import type { Config } from "typescript-eslint"
 
 export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.strict, ...tseslint.configs.stylistic, {
   ignores: ["**/dist"],
-})
+}) satisfies Config
