ee536e81071a00942e2669e5b97b07315fd37ca6
Stevie-Ray Hartog <mail@stevie-ray.nl>
2024-10-02 21:40:23 +0200

    update vitepress ingores

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,5 +3,5 @@ import tseslint from "typescript-eslint"
 import type { Config } from "typescript-eslint"
 
 export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.strict, ...tseslint.configs.stylistic, {
-  ignores: ["**/dist"],
+  ignores: ["**/dist", "**/cache"],
 }) satisfies Config
