fd9600ec8451bd319cc7bfafc0963e4d1a276509
Stevie-Ray Hartog <mail@stevie-ray.nl>
2025-07-15 17:05:18 +0200

    add fallback for random UUID + update version

--- packages/core/src/models/base.model.ts
+++ packages/core/src/models/base.model.ts
@@ -2,15 +2,25 @@ import type { IBase } from "../interfaces/base.interface.js"
 
 export abstract class BaseModel {
   id?: string
-
   createdAt?: Date
-
   updatedAt?: Date
 
   constructor(base: IBase) {
-    this.id = base.id ?? globalThis.crypto?.randomUUID()
+    this.id = base.id ?? globalThis.crypto?.randomUUID?.() ?? BaseModel.generateUUID()
 
     this.createdAt = base.createdAt
     this.updatedAt = base.updatedAt
+  }
+
+  /**
+   * Generates a UUID using a simple algorithm.
+   * @returns A string representing a UUID.
+   */
+  private static generateUUID(): string {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+      const r = Math.floor(Math.random() * 16)
+      const v = c === "x" ? r : (r & 0x3) | 0x8
+      return v.toString(16)
+    })
   }
 }
